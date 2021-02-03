e21359281a3394fa7e9880985934a14f32178a72
Maxim <etsuokiplay@gmail.com>
2021-02-03 17:36:17 +0300

    changes-2

--- dist/js/index.js
+++ dist/js/index.js
@@ -181,7 +181,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }, {
             key: 'serverRequest',
             value: function serverRequest(data) {
-                var serverLink = "http://localhost:8000/";
+                var serverLink = "https://frozen-reef-89470.herokuapp.com";
 
                 var xhr = new XMLHttpRequest();
                 xhr.open("POST", serverLink, false);
